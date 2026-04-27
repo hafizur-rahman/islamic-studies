148085c9459f6bd4bb17f2253ad747311def684e
Hafizur Rahman <bibagimon@gmail.com>
2026-04-27 12:20:44 +0900

    Refactoring

--- src/main/java/com/jdreamer/repository/BookRepository.java
+++ src/main/java/com/jdreamer/repository/BookRepository.java
@@ -4,7 +4,7 @@ import com.jdreamer.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//@Repository
+@Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
     Book findByFilePath(String filePath);
 }
--- src/main/java/com/jdreamer/repository/VerbRepository.java
+++ src/main/java/com/jdreamer/repository/VerbRepository.java
@@ -4,5 +4,6 @@ import com.jdreamer.model.Verb;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VerbRepository extends JpaRepository<Verb, Integer> {
 }
