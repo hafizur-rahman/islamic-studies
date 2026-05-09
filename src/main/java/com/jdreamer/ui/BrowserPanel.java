45d3c459ade6c3498173e6314a31cefc13fee006
Hafizur Rahman <bibagimon@gmail.com>
2026-05-09 12:34:12 +0900

    Refactoring

--- src/main/java/com/jdreamer/ui/BrowserPanel.java
+++ src/main/java/com/jdreamer/ui/BrowserPanel.java
@@ -291,6 +291,7 @@ public class BrowserPanel extends JFXPanel {
             }
 
             mediaPlayer = prepareMediaPlayer(url);
+            mediaPlayer.play();
 
             setScene(mediaPlayerScene);
         });
